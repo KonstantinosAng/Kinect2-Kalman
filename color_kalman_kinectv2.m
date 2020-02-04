0d0c3704cdfec53b1e5ec52657905c3eca18882b
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2020-02-05 01:45:41 +0200

    Cleaned Code

--- color_kalman_kinectv2.m
+++ color_kalman_kinectv2.m
@@ -5,27 +5,12 @@ clear;
 clc; 
 close all;
 
-
-%% Calibrate Camera with chessboard images
-% cameraCalibrator % open calibrator
-
-
 %% create color and depth kinect video input objects
 depthVid = videoinput('kinect', 2, 'Depth_512x424'); %start depth video
 colorVid = videoinput('kinect', 1, 'BGR_1920x1080'); %start color video
 triggerconfig(depthVid,'manual'); %manual triger video
 triggerconfig(colorVid,'manual'); %manual trigger video
 counter = 1; % Counter to store data
-% depth_focal_length = 365.7; % focal length for depth map from pdf https://www.uv.es/imaging3/PDFs/2016_OEng_56_41305.pdf
-% color_focal_length = 329.1; % focal length for colormap from pdf https://www.uv.es/imaging3/PDFs/2016_OEng_56_41305.pdf
-% depth_FOV_horizontal = 71; %in pixels http://www.smeenk.com/webgl/kinectfovexplorer.html
-% color_FOV_horizontal = 84; %in pixels http://www.smeenk.com/webgl/kinectfovexplorer.html
-% depth_focal_length = (524*0.5)/tan(depth_FOV_horizontal*0.5*pi/180); %https://answers.opencv.org/question/17076/conversion-focal-distance-from-mm-to-pixels/
-% color_focal_length = (1920*0.5)/tan(color_FOV_horizontal*0.5*pi/180);%https://answers.opencv.org/question/17076/conversion-focal-distance-from-mm-to-pixels/
-% depth_focal_length_x = 391.096; %https://threeconstants.wordpress.com/2014/11/09/kinect-v2-depth-camera-calibration/
-% depth_focal_length_y = 463.098; %https://threeconstants.wordpress.com/2014/11/09/kinect-v2-depth-camera-calibration/
-% color_focal_length_x = 1062.48808; %https://www.semanticscholar.org/paper/A-new-method-in-simultaneous-estimation-of-sensor-Safaei-Fazli/3944c7f5e39090f63ccbc4ac5664519003a3cedf/figure/4
-% color_focal_length_y = 1065.01486; %https://www.semanticscholar.org/paper/A-new-method-in-simultaneous-estimation-of-sensor-Safaei-Fazli/3944c7f5e39090f63ccbc4ac5664519003a3cedf/figure/4
 depth_focal_length_x = 388.198; %https://www.researchgate.net/figure/Parameters-from-the-calibration-of-the-Kinect-v2-by-rounding_tbl4_321048476
 depth_focal_length_y = 389.033; %https://www.researchgate.net/figure/Parameters-from-the-calibration-of-the-Kinect-v2-by-rounding_tbl4_321048476
 color_focal_length_x = 1144.361; %https://www.researchgate.net/figure/Parameters-from-the-calibration-of-the-Kinect-v2-by-rounding_tbl4_321048476
@@ -81,9 +66,6 @@ Q_loc_meas = []; %real path
 u = .005;
 noise_mag = 4; %noise for velocity how fast is speeding up
 
-% noise_x = 0.02547; %mean of stamdard deviation of kinect in x axis position https://www.researchgate.net/publication/286624514_Performance_evaluation_of_the_1st_and_2nd_generation_Kinect_for_multimedia_applications
-% noise_y = 0.02547; %mean of standard deviation of kinect in y axis position https://www.researchgate.net/publication/286624514_Performance_evaluation_of_the_1st_and_2nd_generation_Kinect_for_multimedia_applications
-% noise_z = 0.02547; %mean of standard deviation of kinect in z axis position https://www.researchgate.net/publication/286624514_Performance_evaluation_of_the_1st_and_2nd_generation_Kinect_for_multimedia_applications
 noise_x = 10; noise_y = 10; noise_z = 10; % rule of thumb ---> more noise is better
 
 Ez = [noise_x 0 0; ...
@@ -136,9 +118,7 @@ P_estimate = P;
 %% Start mapping skeleton   
 
 while ishandle(himg) %loop for every frame
-%     tic
     trigger(depthVid); %start triggering depth stream 1 frame per second
-%     dt = toc; %refresh rate is set to the time needed to obtain frame
     trigger(colorVid); %start triggering color stream 1 frame per second
     [depthMap, depthTime, depthMetaData] = getdata(depthVid); %get data from video
     [colorMap] = getdata(colorVid); % get colordata
@@ -149,10 +129,8 @@ while ishandle(himg) %loop for every frame
     depthWidth = size(depthMap ,2); % find depth Width
     colors = ['r';'g';'b';'c';'y';'m']; % colors for different bodies
     imshow(colorMap); %display color frame
-%     imshow(depthMap, [0 4096]); % display depth frame
        if (sum(bodies)) > 0 % if body is tracked
             imshow(colorMap); %display color frame
-%             imshow(depthMap, [0 4096]); % display depth frame
             skeletonJoints = depthMetaData.DepthJointIndices(:,:,depthMetaData.IsBodyTracked); % get the skeleton joints
             posJoints = depthMetaData.JointPositions(:,:,depthMetaData.IsBodyTracked); % get the real positions of tracked joints in meters
             hand_state = depthMetaData.HandRightState(trackedBodies); % 0 unknown | 1 not tracked | 2 open | 3 closed | 4 lasso
@@ -210,22 +188,12 @@ while ishandle(himg) %loop for every frame
                 rot_y = (acos((line_v(1,2).*y_axis(1,2))/(norm(line_v)*norm(y_axis))))*(180/pi);   %degrees
                 rot_z = (acos((line_v(1,3).*z_axis(1,3))/(norm(line_v)*norm(z_axis))))*(180/pi);   %degrees
 
-                % Print orientation to axis
-%                     text('units','pixels','position',[100 70],'color','red','fontsize',20,'string','X:');
-%                     text('units','pixels','position',[130 70],'color','red','fontsize',20,'string',num2str(rot_x));
-%                     text('units','pixels','position',[100 45],'color','red','fontsize',20,'string','Y:');
-%                     text('units','pixels','position',[130 45],'color','red','fontsize',20,'string',num2str(rot_y));
-%                     text('units','pixels','position',[100 20],'color','red','fontsize',20,'string','Z:');
-%                     text('units','pixels','position',[130 20],'color','red','fontsize',20,'string',num2str(rot_z));
-
                 % Plot circle around hand in depth coordinates
                     if (flagStart == 1)
                         th = 0:pi/50:2*pi;
                         rd = round(90 - 1e3*min(hand_pos_z)/30);
                         coords = depthMetaData.ColorJointIndices(12,:,trackedBodies); %coordinates for color Map
                         plot(rd*sin(th) + coords(1,1) , rd*cos(th) + coords(1,2),'g'); %plot for color map
-    %                         coords = depthMetaData.DepthJointIndices(25,:,trackedBodies); % coordinates for depth Map
-    %                         plot(rd*sin(th) + coords(1,1),rd*cos(th) + coords(1,2),'g'); %plot for depth Map
     %                         plot(skeletonJoints(12,1,body), skeletonJoints(12,2,body), 'r*');
                     end
                 end
@@ -259,13 +227,6 @@ while ishandle(himg) %loop for every frame
                 acceleration_z(counter) = velocity_z(counter)/sum_time; %acceleration at z axis (m/sec2)
             end
 
-
-%                 % Autoencoder prediction
-%                 autoencoder = trainAutoencoder([f_thumb_x(counter);f_thumb_y(counter);f_thumb_z(counter)],hidden_size,'MaxEpochs',60,'ScaleData',0,'ShowProgressWindow',false,'DecoderTransferFunction','purelin');
-%                 pred(:,counter) = predict(autoencoder,[f_thumb_x(counter);f_thumb_y(counter);f_thumb_z(counter)]);
-%                 mseError(counter) = mse([f_thumb_x(counter);f_thumb_y(counter);f_thumb_z(counter)] - pred);
-%                 
-
             % Kalman filter estimation (Linear)
             % Estimation
             if (counter == 1)
@@ -273,7 +234,7 @@ while ishandle(himg) %loop for every frame
             end
 
             Q_loc_meas(:,counter) = [hand_x(counter);hand_y(counter);hand_z(counter)]; %Real location from kinect
-%                 u = norm([velocity_x(counter) velocity_y(counter) velocity_z(counter)]); %norm of velocity of thumb
+			
             u = norm([acceleration_x(counter) acceleration_y(counter) acceleration_z(counter)]); %norm of acceleration of thumb
 
             Q_estimate = A * Q_estimate + B * u; % Predicted next state (linear)
@@ -303,29 +264,16 @@ while ishandle(himg) %loop for every frame
             color_v = -((color_focal_length_y * Q_estimate(2) / Q_estimate(3)) - 1080/2);
             
             if (flagStart == 1)
-%                 plot(rd*sin(th)+depth_u,rd*cos(th)+depth_v,'r'); % the kalman filter tracking in depth map
                 plot(rd*sin(th)+color_u,rd*cos(th)+color_v,'r'); % the kalman filter tracking in color map
             end
 
             hold off; % release frame
 
+            counter = counter + 1; %counter for storing
+            time(counter) = time(counter-1) + frame_dt;
+            flushdata(depthVid); %flush frames to free memory
+            flushdata(colorVid); % flush frames to free ram 
 
-%                 disp('Hand State');
-%                 disp(hand_state(trackedBodies));
-%                 disp('Kinect Location');
-%                 disp(double(Q_loc_meas(1,counter)));
-%                 disp(double(Q_loc_meas(2,counter)));
-%                 disp(double(Q_loc_meas(3,counter)));
-%                 disp('Kalman prediction');
-%                 disp(double(Q_estimate(1)));
-%                 disp(double(Q_estimate(2)));
-%                 disp(double(Q_estimate(3)));
-              counter = counter + 1; %counter for storing
-              time(counter) = time(counter-1) + frame_dt;
-              flushdata(depthVid); %flush frames to free memory
-              flushdata(colorVid); % flush frames to free ram 
-    %             flushdata(depthVid,'triggers'); %flush one frame per
-    %             time
         end
         if (counter == 70)
             break % break after a number of points
